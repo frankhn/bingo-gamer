3936346023ef62b16c661f7189764f0f4d2d2d80
frankhn <harfrank2@gmail.com>
2022-05-03 00:25:11 +0200

    adds guess context

---
+++ src/context/guess.context.tsx
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useMemo, useState } from 'react';
+
+const GuessContext = createContext({ guess: 0, marked: []});
+
+export const useGuess = () => useContext(GuessContext);
+
+export const GuessContextProvider = ({ children }: any) => {
+  const [guess, setGuess] = useState({ guess: 0, marked: [] });
+
+  const providerValue: any = useMemo(() => ({ guess, setGuess }), [guess, setGuess]);
+
+  return (
+    <GuessContext.Provider value={providerValue} >
+      {children}
+    </GuessContext.Provider>
+  );
+};
