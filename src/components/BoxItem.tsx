0b0bf6b8f479c43399438870275a1f21f51f8f60
frankhn <harfrank2@gmail.com>
2022-05-03 00:24:25 +0200

    adds box item component

--- src/components/BoxItem.tsx
+++ src/components/BoxItem.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useGuess } from '../context/guess.context';
 import boxes from '../utils/data/boxes';
 
 function BoxItem({ text, id }: { text: string, id: number }) {
+
+    const { guess, setGuess }: any = useGuess()
+
     const centerBingoBox = () => (
-        <div className="border-2 py-6 px-2" onClick={() => console.log("something", boxes[id])}>
+        <div key={id} className="border-2 py-6 px-2" onClick={() => console.log("something", boxes[id])}>
             {text}
         </div>
     );
@@ -14,12 +18,13 @@ function BoxItem({ text, id }: { text: string, id: number }) {
 
     return (
         <>
-            <div className="w-32 h-32 text-center border-2  py-6 px-2 whitespace-wrap " onClick={() => console.log("something", id)}>
+            <div key={id} className="w-32 h-32 text-center border-2  py-6 px-2 whitespace-wrap "
+                onClick={() => console.log("something", id)}>
                 {
                     id === getCenter()
                         ?
                         "Call Bingo"
-                        : `${text} ${id}`
+                        : `${id}`
                 }
             </div>
         </>
