2b917ebb76fae3e0ff88505221a2954681e9c3dd
frankhn <harfrank2@gmail.com>
2022-05-03 15:04:29 +0200

    adds boxes context

--- src/context/boxes.context.tsx
+++ src/context/boxes.context.tsx
@@ -1,11 +1,11 @@
-import React, { createContext, useContext, useMemo, useState } from 'react';
+import { createContext, useContext, useMemo, useState } from 'react';
 
-const BoxesContext = createContext({ Boxes: 0, marked: [] });
+const BoxesContext = createContext({ boxes: 0, marked: [] });
 
 export const useBoxes = () => useContext(BoxesContext);
 
 export const BoxesContextProvider = ({ children }: any) => {
-    const [boxes, setBoxes] = useState({ Boxes: 0, marked: [] });
+    const [boxes, setBoxes] = useState({ boxes: 0, marked: [] });
 
     const providerValue: any = useMemo(() => ({ boxes, setBoxes }), [boxes, setBoxes]);
 
