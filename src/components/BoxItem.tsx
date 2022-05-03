6a5575de1f623f2d7931dc7bdf20bc710169940e
frankhn <harfrank2@gmail.com>
2022-05-03 15:03:14 +0200

    adds filter for  marked bingos

--- src/components/BoxItem.tsx
+++ src/components/BoxItem.tsx
@@ -1,32 +1,25 @@
-import React from 'react';
 import { useGuess } from '../context/guess.context';
-import boxes from '../utils/data/boxes';
+import { Iboxes } from '../interfaces/boxes.interface';
 
-function BoxItem({ text, id }: { text: string, id: number }) {
+function BoxItem({ text, id, marked }: Iboxes) {
 
     const { guess, setGuess }: any = useGuess()
 
-    const centerBingoBox = () => (
-        <div key={id} className="border-2 py-6 px-2" onClick={() => console.log("something", boxes[id])}>
-            {text}
-        </div>
-    );
-
-    const getCenter = () => {
-        return Math.ceil(boxes.length / 2)
-    }
-
     return (
         <>
-            <div key={id} className="w-32 h-32 text-center border-2  py-6 px-2 whitespace-wrap "
-                onClick={() => console.log("something", id)}>
-                {
-                    id === getCenter()
-                        ?
-                        "Call Bingo"
-                        : `${id}`
-                }
-            </div>
+            {
+                guess.marked.includes(id)
+                    ? <div key={id} className="w-32 h-32 text-center border-2 bg-gray-300 py-6 px-2 whitespace-wrap">
+                        {
+                            `${text}`
+                        }
+                    </div>
+                    : <div key={id} className="w-32 h-32 text-center border-2 py-6 px-2 whitespace-wrap">
+                        {
+                            `${id}`
+                        }
+                    </div>
+            }
         </>
     );
 }
